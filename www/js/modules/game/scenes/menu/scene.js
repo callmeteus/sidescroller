afdb268a4b0d8820cb134a083a8470ed35531588
Matheus Giovani <3390576+theprometeus@users.noreply.github.com>
2018-06-07 18:40:22 -0300

    Menu scene function complexity
    Fixed Menu scene function complexity

--- www/js/modules/game/scenes/menu/scene.js
+++ www/js/modules/game/scenes/menu/scene.js
@@ -1,26 +1,28 @@
 Sidescroller.Scenes.menu 		= function(callback) {
 	// Load game container
-	app_mustache_load("game", document.body, null, true, function() {
-		// Load all unlocked stages
-		Sidescroller.Stages.get(function() {
-			// Load menu
-			app_mustache_load("menu", $container, null, false, function() {
-				// Check if game is loaded
-				if (typeof Sidescroller.Game.layers !== "undefined")
-					// Hide game
-					game_fadeOut(function() {
-						// Reset game stage
-						Sidescroller.Stages.reset();
+	app_mustache_load("game", document.body, null, true);
 
-						// Recreate the game
-						Sidescroller.Scenes.create();
-					});
+	// Load all unlocked stages
+	Sidescroller.Stages.get(function() {
+		// Load menu
+		app_mustache_load("menu", $container, null, false, function() {
+			if (typeof callback === "function")
+				callback();
+			else
+				$container.find("#game-menu").modal("show");
 
-				if (typeof callback === "function")
-					callback();
-				else
-					$container.find("#game-menu").modal("show");
+			// Check if game is loaded
+			if (typeof Sidescroller.Game.layers !== "undefined")
+				return;
+
+			// Hide game
+			game_fadeOut(function() {
+				// Reset game stage
+				Sidescroller.Stages.reset();
+
+				// Recreate the game
+				Sidescroller.Scenes.create();
 			});
-		});		
+		});
 	});
 };
